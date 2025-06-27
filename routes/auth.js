5f6027d29e10c0c2e72ac6e7a3e4632bfca3fdc9
vamsi6893 <nvamsiram@gmail.com>
2025-06-27 12:35:12 +0530

    add

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post('/login', async (req, res) => {
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) return res.status(400).json({ message: 'Invalid credentials' });
-    const token = jwt.sign({ username: user.username, email: user.email }, JWT_SECRET, { expiresIn: '2h' });
+    const token = jwt.sign({ username: user.username, email: user.email }, JWT_SECRET, { expiresIn: '1000' });
     res.json({ token });
 });
 
