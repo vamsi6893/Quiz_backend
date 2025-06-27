d81492f40543d6366880e1351525445c87669ae6
vamsi6893 <nvamsiram@gmail.com>
2025-06-27 10:13:49 +0530

    add

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 // const allowedOrigins = ['http://localhost:5173', 'https://brain-battle-kappa.vercel.app'];
 
 app.use(cors({
-  origin:"*", // Use "*" to allow all origins, or specify allowedOrigins array
+  origin:[
+    "http://localhost:5173",
+    "https://brain-battle-kappa.vercel.app"
+  ], // Use "*" to allow all origins, or specify allowedOrigins array
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //
   credentials: true // Optional: if you're using cookies or HTTP auth
 }));
